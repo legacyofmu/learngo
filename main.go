72318e896e5a9d49b83f376bde8d7f9dc5b27ae8
legacyofmu <legacyofmu1@gmail.com>
2020-11-13 00:21:36 +0900

    Main

--- main.go
+++ main.go
@@ -2,12 +2,92 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/legacyofmu/learngo/something"
+	"strings"
 )
 
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+// naked return
+// defer : function이 끝났을때 실행되는 함수 지정
+func lenAndUpper2(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	//return len(name), strings.ToUpper(name)
+	return
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers { // _ : index
+		//fmt.Println(index, number)
+		total += number
+	}
+	return total
+}
+
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	}
+	return true
+}
+
+func low() {
+	a := 2
+	b := a
+	a = 10
+	fmt.Println(a, b)
+	fmt.Println(&a, &b) // & : address
+
+	a = 2
+	c := &a // c : pointer to a
+	a = 10
+	fmt.Println(a, *c)
+	fmt.Println(&a, c)
+}
+
+func testArray() {
+	names := []string{"dhpark", "lynn", "dal", "marl"}
+	names = append(names, "flynn")
+	fmt.Println(names)
+}
+
+func testMap() {
+	dhpark := map[string]string{"name": "dhpark", "age": "12"}
+	fmt.Println(dhpark)
+	for key, value := range dhpark {
+		fmt.Println(key, value)
+	}
+}
+
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
+func testStruct() {
+	favFood := []string{"kimchi", "ramen"}
+	dhpark := person{"dhpark", 18, favFood}
+	fmt.Println(dhpark.name)
+}
+
 func main() {
-	fmt.Println("Hello")
-	something.SayHello()
-	something.SayBye()
+	totalLength, upperName := lenAndUpper2("dhpark")
+	fmt.Println(totalLength, upperName)
+	repeatMe("dhpark", "lynn", "dal", "marl", "flynn")
+	total := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(total)
+	fmt.Println(canIDrink(16))
+	low()
+	testArray()
+	testMap()
+	testStruct()
 }
